7bcbe54c9ba4abaa0eb47903372bc81d318c206e
Jordane Brosset <jordane.brosset@gmail.com>
2025-08-25 12:31:02 +0200

    feat: add CI/CD workflows for building and deploying frontend and backend services

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "azurerm" {
+  features {}
+}
+
+# Resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+# Azure Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksprojet"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
+
+# Output kubeconfig
+output "kubeconfig" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "rg_name" {
+  default = "rg-projet"
+}
+
+variable "location" {
+  default = "westeurope"
+}
+
+variable "acr_name" {
+  default = "projetacr123"
+}
+
+variable "aks_name" {
+  default = "aks-projet"
+}
