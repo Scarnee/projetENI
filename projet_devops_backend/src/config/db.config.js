17c495fcf5638ee5d3a1faa4420267acc27a4017
Jordane Brosset <jordane.brosset@gmail.com>
2025-08-26 10:57:31 +0200

    fix: update database configuration to use environment variables

--- projet_devops_backend/src/config/db.config.js
+++ projet_devops_backend/src/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  HOST: 'localhost',
-  USER: 'root',
-  PASSWORD: '',
-  DB: 'todolist_db',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    HOST: process.env.DB_HOST || "mysql-svc", // le service MySQL dans K8s
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "your_password_here",
+    DB: process.env.DB_NAME || "todolist_db",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
 };
